db794ab47d92eaa4f704106bb7147b7e459424a7
becausefrancis <jimenez.francis92@gmail.com>
2025-01-09 08:56:38 -0600

    Added comment section.

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -70,6 +70,28 @@ router.get('/:hootId', async (req, res) => {
     }
 });
 
+router.put('/:hootId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId)
+
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+    const updatedHoot = await Hoot.findByIdAndUpdate(
+      req.params.hootId,
+      req.body,
+      { new: true }
+    )
+
+    updatedHoot._doc.author = req.user;
+
+    res.status(200).json(updatedHoot);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 router.put('/:hootId/comments/:commentId', async (req, res) => {
   try {
     const hoot = await Hoot.findById(req.params.hootId);
@@ -79,6 +101,21 @@ router.put('/:hootId/comments/:commentId', async (req, res) => {
     res.status(200).json({ message: 'Ok' });
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+router.delete('/:hootId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    };
+
+    const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+    res.status(200).json(deletedHoot);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
